4ae36c6ea778810b9d186575cfa7b6dd7227e9e9
MariaCArroyoA <camila.arroyoa@gmail.com>
2025-05-05 04:52:20 -0500

    deploy test

---
+++ Batch-lambda/index.ts
@@ -0,0 +1,9 @@
+import { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event, context) => {
+  console.log("Lambda ejecutada. Evento recibido:", JSON.stringify(event));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Lambda ejecutada correctamente." })
+  };
+};
